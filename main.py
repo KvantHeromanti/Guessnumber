a3fd387e40ad67812327b98e3e20402e18a90a03
KvantHeromanti <Kvantdiskk@yandex.ru>
2020-10-19 10:27:45 +1000

    -

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ numeral = ''
 move = 1
 block = 0
 start = 1
-OUTSIDE_BG = (0, -100)
+OUTSIDE_BG = (-100, -100)
 
 pygame.init()
 pygame.display.set_caption('GUESS NUMBER!')
 screen = pygame.display.set_mode((W, H))
-pygame.mouse.set_visible(False)
+pygame.mouse.set_visible(True)
 
 font = pygame.font.SysFont('Arial', 28, True, False)
 font_box = pygame.Surface((W - font.get_height(), font.get_height()))
@@ -38,7 +38,18 @@ owl_rect = owl.get_rect(center=(210, 120))
 dialog = pygame.image.load('Image/dialog.png')
 dialog_rect = dialog.get_rect()
 dialog_cat_pos = (cat_rect.x, cat_rect.y - dialog_rect.h)
-dialog_dog_pos = (dog_rect.x, dog_rect.w // 2, dog_rect.y - dialog_rect.h)
+dialog_dog_pos = (dog_rect.x - dialog_rect.w // 2, dog_rect.y - dialog_rect.h)
+dialog_owl_pos = (owl_rect.x, owl_rect.y - dialog_rect.h)
+
+
+def dialogs(text, pos, owl_text):
+    screen.blit(dialog, pos)
+    screen.blit(font2.render(text, True, BLACK), (pos[0] + 5, pos[1] + 5))
+    screen.blit(dialog, dialog_owl_pos)
+    screen.blit(font2.render(owl_text, True, BLACK), (dialog_owl_pos[0] + 5, dialog_owl_pos[1] + 5))
+    pygame.display.update()
+    pygame.time.wait(2000)
+
 
 run = True
 while run:
@@ -48,8 +59,21 @@ while run:
         elif e.type == pygame.KEYDOWN:
             if e.key == pygame.K_ESCAPE:
                 run = False
-    screen.blit(bg, bg_rect)
-    screen.blit(cat, cat_rect)
-    screen.blit(dog, dog_rect)
-    screen.blit(owl, owl_rect)
+            elif e.unicode.isdecimal() and block == 0:
+                numeral += e.unicode
+
+    if block == 0:
+        screen.blit(bg, bg_rect)
+        screen.blit(cat, cat_rect)
+        screen.blit(dog, dog_rect)
+        screen.blit(owl, owl_rect)
+        screen.blit(font_box, font_rect)
+        font_box.fill(SILVER)
+        font_box.blit(font.render(numeral, True, BLACK), (10, 0))
     pygame.display.update()
+
+    if start == 1:
+        dialogs('', OUTSIDE_BG, 'Я загадала число')
+        dialogs('', OUTSIDE_BG, 'От 0 до 100')
+        dialogs('Кот, твой ход', dialog_dog_pos, 'Отгадайте его')
+        start = 0
